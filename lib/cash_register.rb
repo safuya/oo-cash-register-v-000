70a96a6bb9a73b981cf195341d8bb98eea763791
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 04:15:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def apply_discount
-    (@total.to_f * (100.0 - @discount.to_f) / 100.0).to_i
+    @total = (@total.to_f * (100.0 - @discount.to_f) / 100.0).to_i
   end
 
 end
